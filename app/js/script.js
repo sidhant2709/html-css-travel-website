a09e3a7c09d7048f1d98d233e8466196f54d657e
Sidhant <sidhant1593@gmail.com>
2023-04-15 01:48:53 +0530

    packages updated

--- app/js/script.js
+++ app/js/script.js
@@ -2,7 +2,7 @@ const testString = "This is a test!";
 
 console.log(testString);
 
-const fruits = ["mango", "papaya", "lychee"];
+const fruits = ["mango", "papaya", "lychee", "mango", "papaya", "lychee"];
 
 fruits.map((fruit) => {
   console.log(fruit);
